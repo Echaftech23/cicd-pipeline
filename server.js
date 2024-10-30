286bc552ec7a5201a574360602d1d09415f513a5
Rachid Echafai <rachidchaf2001@gmail.com>
2024-10-30 11:48:04 +0100

    initial commit.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.json({
+        status:200,
+        message: 'Hello World'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
